56cde5ed21b26a8c42515c030eea5b5917618369
MAWHBots <160210473+MAWHBots@users.noreply.github.com>
2024-07-08 17:30:31 +0530

    Update info.py

--- info.py
+++ info.py
@@ -16,17 +16,17 @@ API_HASH = environ.get('API_HASH')
 BOT_TOKEN = environ.get('BOT_TOKEN')
 
 # Admins, Channels & Users
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', "990262535").split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', "5324831370").split()]
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS',).split()]
-auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
+auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '5324831370').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 auth_channel = environ.get('AUTH_CHANNEL')
-auth_grp = environ.get('AUTH_GROUP', "-1001633071686")
+auth_grp = environ.get('AUTH_GROUP', "-1002239809844")
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 SUPPORT_CHANNEL = environ.get('CHANNEL_USERNAME')
 SUPPORT_GROUP = environ.get('GROUP_USERNAME')
-INDEX_USER = [int(environ.get('INDEX_USER', '1247742004'))]
+INDEX_USER = [int(environ.get('INDEX_USER', ''))]
 INDEX_USER.extend(ADMINS)
 
 # MongoDB information
@@ -39,7 +39,7 @@ LOG_CHANNEL = int(environ.get('LOG_CHANNEL'))
 FORCESUB_CHANNEL = int(environ.get('FORCESUB_CHANNEL'))
 SLOW_MODE_DELAY = int(environ.get('SLOW_MODE_DELAY', 60))
 WAIT_TIME = int(environ.get('AUTO_DELETE_WAIT_TIME', 1800))
-FORWARD_CHANNEL = int(environ.get('FORWARD_CHANNEL', "-1002123504264"))
+FORWARD_CHANNEL = int(environ.get('FORWARD_CHANNEL', "-1002029611411"))
 
 # other
 ACCESS_KEY = environ.get("LICENSE_ACCESS_KEY")
